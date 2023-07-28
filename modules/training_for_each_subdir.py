30a5dc7e5fb349ad4bc1de12efe7cf0660e60e04
NG-Bullseye <leonard.r.wecke@gmail.com>
2023-07-28 14:52:40 +0200

    -compatibility issue fixed. Tensorflow 2.4.0 and experimental to save and load tf.data

--- modules/training_for_each_subdir.py
+++ modules/training_for_each_subdir.py
@@ -1,5 +1,6 @@
 import os
 from modules import model_training
+from modules.Utility.PrettySafeLoader import PrettySafeLoader
 
 
 def list_subdirectories(directory):
@@ -26,7 +27,34 @@ def list_subdirectories(directory):
 
 
 def main():
+    import os
+    import yaml
+    from datetime import datetime
+
+    PrettySafeLoader.add_constructor(
+        u'tag:yaml.org,2002:python/tuple',
+        PrettySafeLoader.construct_python_tuple)
+    with open('./params.yaml', 'r') as stream:
+        params = yaml.load(stream, Loader=PrettySafeLoader)
+
+    # Get the current time
+    current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+    # Check if "Bulktraining" is present at the end of the path
+    if params['output_training_path'].endswith('/Bulktraining'):
+        # Replace the existing timestamp with the current time
+        params['output_training_path'] = params['output_training_path'][:-(len('/Bulktraining'))] + f'{current_time}'
+    else:
+        # Append "/Bulktraining<currentTime>"
+        params['output_training_path'] = os.path.join(params['output_training_path'], f'Bulktraining{current_time}')
+
+    # Save the modified data back to the YAML file
+    with open('./params.yaml', 'w') as file:
+        yaml.dump(params, file)
     # Example usage:
+    with open('./params.yaml', 'r') as stream:
+        params = yaml.load(stream, Loader=PrettySafeLoader)
+    bulk_training=    params['output_training_path']
     directory_path = "/home/lwecke/Datensätze/Datensatz_v1_50p_3reg/Datensatz_v1_50p_3reg/yaw"
     yaw_to_accuracy = list_subdirectories(directory_path)
     # print yaw_to_accuracy to check the validation accuracy for each yaw value
